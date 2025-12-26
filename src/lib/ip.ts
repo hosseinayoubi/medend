41dab40673ef0c7a9caeb5031ac9be2d7155038b
Abdolhossein Ayoubi <125464377+hosseinayoubi@users.noreply.github.com>
2025-12-26 14:37:12 +0200

    Update ip.ts

--- src/lib/ip.ts
+++ src/lib/ip.ts
@@ -1,10 +1,9 @@
-// src/lib/ip.ts
 export function getClientIp(req: Request): string {
-  // Vercel / proxies usually set x-forwarded-for as "client, proxy1, proxy2"
   const xff = req.headers.get("x-forwarded-for");
-  const ipFromXff = xff?.split(",")[0]?.trim();
+  if (xff) return xff.split(",")[0].trim();
 
-  const xRealIp = req.headers.get("x-real-ip")?.trim();
+  const xrip = req.headers.get("x-real-ip");
+  if (xrip) return xrip.trim();
 
-  return ipFromXff || xRealIp || "unknown";
+  return "unknown";
 }
